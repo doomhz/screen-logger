7c97f2aa0a070a48e96f64e70884cd50e36b7976
Dumitru Glavan <dumitru.gl@gmail.com>
2012-12-23 16:32:58 +0100

    Add the screen tracker

---
+++ screen_logger.coffee
@@ -0,0 +1,61 @@
+###
+
+  ScreenLogger
+
+  @version 1.0
+  @author  Dumitru Glavan
+  @link    http://jslogger.com
+  @link    http://dumitruglavan.com
+
+###
+
+class window.ScreenLogger
+
+  logger: null
+
+  html2CanvasUrl: "//jslogger.com/html2canvas.js"
+
+  constructor: (options = {})->
+    @logger = options.logger
+    @session = options.session or new Date().getTime()
+    @loadDependencies @setupEvents
+
+  loadDependencies: (callback)->
+    if not window.html2canvas
+      script = document.createElement("script")
+      script.type = "text/javascript"
+      script.src = @html2CanvasUrl
+      script.onload = callback
+      document.getElementsByTagName("body")[0].appendChild(script)
+    else
+      callback() if typeof callback isnt undefined
+
+  setupEvents: ()->
+    document.onclick = @onClick
+
+  log: (data, extraParams)->
+    @logger.logDataByType("screen", data, extraParams) if @logger and @logger.track
+
+  takeScreenshot: (callback)->
+    opts =
+      logging: true
+      #profile: true,
+      useCORS: true
+      onrendered: (canvas)->
+        try
+          content = canvas.toDataURL()
+          callback(content)
+        catch e
+          console.log(e)
+          console.log(canvas)
+    html2canvas(document.getElementsByTagName("body"), opts)
+
+  onClick: (e)=>
+    extraParams =
+      event:
+        type: "click"
+        x: e.clientX
+        y: e.clientY
+    @takeScreenshot (image)=>
+      extraParams.img = image
+      @log(@session, extraParams)
---
+++ screen_logger.js
@@ -0,0 +1,91 @@
+
+/*
+
+  ScreenLogger
+
+  @version 1.0
+  @author  Dumitru Glavan
+  @link    http://jslogger.com
+  @link    http://dumitruglavan.com
+*/
+
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  window.ScreenLogger = (function() {
+
+    ScreenLogger.prototype.logger = null;
+
+    ScreenLogger.prototype.html2CanvasUrl = "//jslogger.com/html2canvas.js";
+
+    function ScreenLogger(options) {
+      if (options == null) options = {};
+      this.onClick = __bind(this.onClick, this);
+      this.logger = options.logger;
+      this.session = options.session || new Date().getTime();
+      this.loadDependencies(this.setupEvents);
+    }
+
+    ScreenLogger.prototype.loadDependencies = function(callback) {
+      var script;
+      if (!window.html2canvas) {
+        script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = this.html2CanvasUrl;
+        script.onload = callback;
+        return document.getElementsByTagName("body")[0].appendChild(script);
+      } else {
+        if (typeof callback !== void 0) return callback();
+      }
+    };
+
+    ScreenLogger.prototype.setupEvents = function() {
+      return document.onclick = this.onClick;
+    };
+
+    ScreenLogger.prototype.log = function(data, extraParams) {
+      if (this.logger && this.logger.track) {
+        return this.logger.logDataByType("screen", data, extraParams);
+      }
+    };
+
+    ScreenLogger.prototype.takeScreenshot = function(callback) {
+      var opts;
+      opts = {
+        logging: true,
+        useCORS: true,
+        onrendered: function(canvas) {
+          var content;
+          try {
+            content = canvas.toDataURL();
+            return callback(content);
+          } catch (e) {
+            console.log(e);
+            return console.log(canvas);
+          }
+        }
+      };
+      return html2canvas(document.getElementsByTagName("body"), opts);
+    };
+
+    ScreenLogger.prototype.onClick = function(e) {
+      var extraParams,
+        _this = this;
+      extraParams = {
+        event: {
+          type: "click",
+          x: e.clientX,
+          y: e.clientY
+        }
+      };
+      return this.takeScreenshot(function(image) {
+        extraParams.img = image;
+        return _this.log(_this.session, extraParams);
+      });
+    };
+
+    return ScreenLogger;
+
+  })();
+
+}).call(this);
